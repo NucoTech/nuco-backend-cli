d4ae79d789ea53e93f0b36ebfe51e372e68c8700
HerbertHe <Herbert.He0229@gmail.com>
2020-12-14 18:36:29 +0800

    chore :wrench: : 交叉编译辅助工具

---
+++ gen_new.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# 用于linux环境下交叉编译
+echo "nbc项目交叉编译开始..."
+echo ""
+# Windows
+set GOOS = "windows" && go build -o nbc.exe && echo "Windows平台编译完成!"
+# MacOS
+set GOOS = "darwin" && go build -o nbc.darwin && echo "MacOS平台编译完成!"
+# Linux
+set GOOS = "linux" && go build -o nbc.linux && echo "Linux平台编译完成!"
+echo ""
+echo "nbc项目交叉编译完成!!!"
