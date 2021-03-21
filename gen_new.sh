1ce4f84bae4199fe6e69b18853f73235be59ce31
HerbertHe <Herbert.He0229@gmail.com>
2021-03-21 20:10:33 +0800

    feat :sparkles: : 支持Apple M1芯片, 支持Linux arm64

--- gen_new.sh
+++ gen_new.sh
@@ -7,9 +7,11 @@ echo ""
 export GOARCH="amd64" && GOOS="windows" && go build -o nbc.exe && echo "Windows平台编译完成!"
 # MacOS
 export GOARCH="amd64" && GOOS="darwin" && go build -o nbc.darwin && echo "MacOS平台编译完成!"
+# MacOS arm64
+export GOARCH="arm64" && GOOS="darwin" && go build -o nbc-arm64.darwin && echo "MacOS arm64平台编译完成!"
 # Linux
 export GOARCH="amd64" && GOOS="linux" && go build -o nbc.linux && echo "Linux平台编译完成!"
 # Linux arm64
-export GOARCH="arm64" && GOOS="linux" && go build -o nbc-arm64.linux && echo "Linux arm64 platform compiled!"
+export GOARCH="arm64" && GOOS="linux" && go build -o nbc-arm64.linux && echo "Linux arm64平台编译完成!"
 echo ""
 echo "nbc项目交叉编译完成!!!"
